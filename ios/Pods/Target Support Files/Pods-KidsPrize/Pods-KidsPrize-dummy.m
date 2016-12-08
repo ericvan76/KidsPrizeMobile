80d133986a68554ee4c7a1ce60e72d2cbcd1d37f
Eric Fan <eric.fan@alcidion.com>
2016-12-09 09:20:52 +1030

    gitignore Pods

--- ios/Pods/Target Support Files/Pods-KidsPrize/Pods-KidsPrize-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KidsPrize : NSObject
-@end
-@implementation PodsDummy_Pods_KidsPrize
-@end
--- ios/Pods/Target Support Files/Pods-KidsPrizeTests/Pods-KidsPrizeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KidsPrizeTests : NSObject
-@end
-@implementation PodsDummy_Pods_KidsPrizeTests
-@end

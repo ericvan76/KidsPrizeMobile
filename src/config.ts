83226bbdc509cdbc6769d846eb0254f7c7df8baf
Eric J. Fan <ericvan76@users.noreply.github.com>
2017-11-13 20:21:53 +1030

    bitrise support (#10)

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import { Config } from './types/config';
 
 const config: Config = {
   auth: {
-    auth0_domain: '<auth0_domain>',
-    client_id: '<client_id>',
-    client_secret: '<client_Secret>'
+    auth0_domain: 'AUTH0_DOMAIN',
+    client_id: 'AUTH0_CLIENT_ID',
+    client_secret: 'AUTH0_CLIENT_SECRET'
   },
   api: {
     baseUrl: 'https://kidsprize-api.fanstek.com/v2'
